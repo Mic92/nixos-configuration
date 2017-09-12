d273d56ae8c7ac90bfb3d01fff2cfd74c05f26e5
Jörg Thalheim <joerg@thalheim.io>
2017-09-12 16:22:56 +0100

    deprecation status

---
+++ README.md
@@ -0,0 +1,3 @@
+# NixOS/Nixpkgs Configuration
+
+- The content of this repo has been moved to https://github.com/Mic92/dotfiles/
--- test-vm.nix
+++
@@ -1,8 +0,0 @@
-{ lib, config, pkgs, fetchgit, ... }:
-# nixos-rebuild -I nixos-config=./test-vm.nix build-vm
-{
-  #boot.supportedFilesystems = [ "bcachefs" ];
-  #boot.kernelPackages = pkgs.linuxPackages_testing_bcachefs;
-  programs.zsh.ohMyZsh.enable = true;
-  users.users.root.initialPassword = "root";
-}
